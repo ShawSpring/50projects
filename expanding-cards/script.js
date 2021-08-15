138a3ef7a63fa847307ad081ccda6687d98944ba
Shaw Spring <chunlaixiao@163.com>
2021-08-15 19:34:48 +0800

    add drawHeartWithCss

--- expanding-cards/script.js
+++ expanding-cards/script.js
@@ -1,12 +1,12 @@
 const panels = document.querySelectorAll('.panel')
 
-panels.forEach(panel=>{
-    panel.addEventListener('click',()=>{
-        
-            panels.forEach(panel=>{
-                panel.classList.remove('active')
-            })
-        
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+
+        panels.forEach(panel => {
+            panel.classList.remove('active')
+        })
+
         panel.classList.add('active')
     })
 })
